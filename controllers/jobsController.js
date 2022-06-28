4f305b2d0b7aac1c6c4a79a7d9817a4c628ca6a4
Christoph Fröhlich <christophfroehlich99@web.de>
2022-06-28 08:02:34 +0200

    added permission to edit jobs

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,6 +1,7 @@
 import Job from "../models/Job.js"
 import { StatusCodes } from "http-status-codes"
-import {BadRequestError, UnauthenticatedError} from "../errors/index.js"
+import {BadRequestError, NotFoundError, UnauthenticatedError} from "../errors/index.js"
+import checkPermissons from "../utils/checkPermissions.js"
 
 
 
@@ -16,7 +17,29 @@ const createJob = async(req,res) => {
 }
 
 const getAllJobs = async (req,res) => {
-    res.send("here are all your jobs")
+    const jobs = await Job.find({createdBy:req.user.userId})
+    res.status(StatusCodes.OK).json({jobs, totalJobs:jobs.length, numOfPages: 1})
+    
+}
+
+const updateJob = async (req,res) => {
+    const {id:jobId} = req.params
+    const {company, position} = req.body
+    if(!position || !company){
+        throw new BadRequestError("Please provide all values")
+    }
+    const job = await Job.findOne({_id: jobId})
+    if(!job){
+        throw new NotFoundError(`No job with id :${jobId}`)
+    }
+
+   checkPermissons(req.user, job.createdBy)
+
+    const updatedJob = await Job.findOneAndUpdate({_id:jobId}, req.body,{
+        new: true,
+        runValidators:true
+    })
+    res.status(StatusCodes.OK).json({updatedJob})
 }
 
 const showStats = async (req,res) => {
@@ -27,9 +50,6 @@ const deleteJob = async (req,res) => {
     res.send("deleted this Job !")
 }
 
-const updateJob = async (req,res) => {
-    res.send("updated this Job")
-}
 
 
 export {createJob,getAllJobs,showStats,deleteJob,updateJob}
