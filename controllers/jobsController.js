23ff6a357b92840c9eedecf9e1104654142b2f8c
Christoph Fröhlich <christophfroehlich99@web.de>
2022-06-29 08:15:59 +0200

    added stats logic

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -2,7 +2,8 @@ import Job from "../models/Job.js"
 import { StatusCodes } from "http-status-codes"
 import {BadRequestError, NotFoundError, UnauthenticatedError} from "../errors/index.js"
 import checkPermissons from "../utils/checkPermissions.js"
-
+import mongoose from "mongoose"
+import moment from "moment"
 
 
 const createJob = async(req,res) => {
@@ -43,11 +44,53 @@ const updateJob = async (req,res) => {
 }
 
 const showStats = async (req,res) => {
-    res.send("Here are all stats")
+    let stats = await Job.aggregate([
+        {$match:{createdBy:mongoose.Types.ObjectId(req.user.userId)}},
+        { $group: { _id: '$status', count:{ $sum: 1}}},
+    ])
+    stats = stats.reduce((acc,curr)=>{
+        const {_id:title,count} = curr
+        acc[title] = count
+        return acc
+    }, {})
+
+    let defaultStats = {
+        pending: stats.pending || 0,
+        interview: stats.interview || 0,
+        declined: stats.declined || 0
+    }
+
+    let monthlyApplications = await Job.aggregate([
+        {$match:{createdBy:mongoose.Types.ObjectId(req.user.userId)}},
+        {$group:{_id:{year:{$year:'$createdAt'}, month:{$month:'$createdAt'}},
+        count:{ $sum : 1}
+    }},
+    {$sort:{'_id.year':-1, '_id.month': -1}},
+    {$limit: 6}
+    ])
+    monthlyApplications= monthlyApplications.map((item) => {
+        const {
+            _id:{year,month},
+            count
+        } = item
+        const date = moment()
+            .month(month - 1)
+            .year(year)
+            .format('MMM Y')
+            return {date, count}
+    }).reverse()
+    res.status(StatusCodes.OK).json({defaultStats, monthlyApplications})
 }
 
 const deleteJob = async (req,res) => {
-    res.send("deleted this Job !")
+    const {id:jobId} = req.params
+    const job = await Job.findOne({_id: jobId})
+    if(!job){
+        throw new NotFoundError(`No job with id :${jobId}`)
+    }
+    checkPermissons(req.user, job.createdBy)
+    job.remove()
+    res.status(StatusCodes.OK).json({msg: "Success! Job removed!}"})
 }
 
 
